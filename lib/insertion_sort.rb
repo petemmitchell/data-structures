a708581770cde473b198d1beaba5a9b83916f9f5
Pete Mitchell <petemmitchell@gmail.com>
2014-10-15 16:04:03 -0700

    Insertion sort working, tests passing. Readme updated.

---
+++ lib/insertion_sort.rb
@@ -0,0 +1,15 @@
+class Sort
+  def self.insertion(arr)
+    # must iterate through all array elements
+    (1..arr.length-1).each do |elem|
+      # end loop if either element 0 or the preceeding element is smaller
+      while elem > 0 && (arr[elem - 1] > arr[elem]) do
+        #swap
+        arr[elem - 1], arr[elem] = arr[elem], arr[elem - 1]
+        elem -= 1
+      end
+    end
+    #display sorted array
+    arr
+  end
+end
---
+++ spec/insertion_spec.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/benchmark'
+require 'insertion_sort'
+require 'benchmark'
+
+describe 'Custom insertion sorting method' do
+  it 'should take an unsorted array and return the array sorted' do
+    @test_array = [17, 2, 31, 7, 88, 6]
+    Sort.insertion(@test_array).must_equal ([2, 6, 7, 17, 31, 88])
+
+  end
+
+  it 'should have benchmarks' do
+      @best_case = (1..5_000).to_a
+      @worst_case = @best_case.reverse
+      @random_case = (1..5_000).to_a.shuffle
+
+    puts 'Best Case (already sorted)'
+    puts Benchmark.measure { Sort.insertion(@best_case) }
+    puts 'Worst Case (reverse sorted)'
+    puts Benchmark.measure { Sort.insertion(@worst_case) }
+    puts 'Random'
+    puts Benchmark.measure { Sort.insertion(@random_case) }
+  end
+end
